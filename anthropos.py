6eb386bb80547b455c0bbef5b4e28fbf3e6c1526
turnq <turnq005@gmail.com>
2020-12-23 17:19:32 -0600

    added gameloop and removed joke variables from anthropos

--- anthropos.py
+++ anthropos.py
@@ -1,9 +1,10 @@
 class Anthropos:
 
-    def slay(self):
+    isAlive = False
+    children = []
+
+    def die(self):
         self.isAlive = False
-        if self.isTheist:
-            self.inAfterLife = True
 
     def beget(self, other):
         other.isAlive = True
@@ -12,17 +13,8 @@ class Anthropos:
     def __init__(self):
         self.isAlive = True
         self.children = []
-        self.inAfterLife = False
-        self.isTheist = True
-
-    def commitApostasy(self):
-        self.isTheist = False
 
 
 
 
-
-dan = Anthropos()
-dan.commitApostasy()
-dan.slay()
 
