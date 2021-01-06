682af98b90cf065defa03f0d89dc3ee4d62bede7
Theokrites <74838406+Theokrites@users.noreply.github.com>
2021-01-05 19:48:32 -0600

    remade Anthropos and created LineagePrinter, which prints out a family tree

--- anthropos.py
+++ anthropos.py
@@ -3,18 +3,28 @@ class Anthropos:
     isAlive = False
     children = []
 
-    def die(self):
+    def die(self, death_eon):
         self.isAlive = False
+        self.eons_lived = death_eon - self.birth_eon
 
     def beget(self, other):
-        other.isAlive = True
-        self.children.append(other)
+        if self.num_children < 2:
+            other.isAlive = True
+            if self.left_child is None:
+                self.left_child = other
+            else:
+                self.right_child = other
+            self.num_children += 1
 
-    def __init__(self):
+    def __init__(self, name, birth_eon):
         self.isAlive = True
-        self.children = []
+        self.left_child = None
+        self.right_child = None
+        self.num_children = 0
+        self.name = name
+        self.birth_eon = birth_eon
+        self.eons_lived = 0
 
 
-#if this can make it to the desktop without a clone I'll call it for now
 
 
