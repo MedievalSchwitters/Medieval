f5e5bcc47b0f131136d5f21de71cbb6ed4652b2f
TailsTyler <tailstylerturnquist@gmail.com>
2021-12-04 13:39:55 -0600

    updated tys_func

--- trying to imrove it.py
+++ trying to imrove it.py
@@ -20,6 +20,7 @@ baby_wait_list = []
 next_parent_in_line_to_have_a_baby = None
 def tys_func():
     print("dis tys func")
+    print("edit by tyler")
 
 # todo make it so people die at certain age and have a childhood in which they cannot reproduce
 # and you can enter peoples initials to sign them out
