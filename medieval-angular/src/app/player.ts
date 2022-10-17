99fddaca01cd5503a6fe6e3ef98f7539ee2c4350
Theocritus <turnq005@gmail.com>
2022-10-16 19:04:46 -0500

    continued work, added lots of person stuff

--- medieval-angular/src/app/player.ts
+++
@@ -1,2 +0,0 @@
-export const Players: string[] = [
-]
--- medieval-angular/src/app/players-list/players-list.component.ts
+++ medieval-angular/src/app/players-list/players-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Players } from '../player';
 import { HttpClient } from '@angular/common/http';
 import { Person } from '../person';
 
@@ -9,14 +8,21 @@ import { Person } from '../person';
   styleUrls: ['./players-list.component.css']
 })
 export class PlayersListComponent implements OnInit {
-
   deadPlayers: string[] = [];
   livingPlayers: string[] = [];
+  people: Person[] = [];
   playersAddedByCSV = false;
   paused: boolean = false;
   protosAnthropos: Person|null = null;
-  
+  eligableProgenitors: Person[] = []; //represents the fertile people who have not yet had a child during this pause
+  livingPeople: Person[] = [];
 
+  //config stuff
+  maxAge = 10;
+  maxNumChildren = 2;
+  adultAge = 3;
+  elderlyAge = 7;
+  
   constructor(private http: HttpClient) { }
 
   addPlayersByCSV(): void{
@@ -27,24 +33,75 @@ export class PlayersListComponent implements OnInit {
   }
 
   alivifyPlayer(player: string): void{
-    this.deadPlayers = this.deadPlayers.filter(name => name !== player);
-    this.livingPlayers.push(player);
+    if (!this.protosAnthropos){
+      console.log(player + " created ex nihlo; may they not eat any forbidden fruits...");
+      this.people = [];
+      this.protosAnthropos = new Person(player);
+      this.people.push(this.protosAnthropos);
+      this.deadPlayers = this.deadPlayers.filter(name => name !== player);
+      this.livingPlayers.push(player);
+      this.livingPeople.push(this.protosAnthropos);
+      return;
+    }
+    if(this.eligableProgenitors.length){
+      let progenitor = this.eligableProgenitors.pop();
+      let child = new Person(player);
+      progenitor?.children.push(child);
+      console.log(child.name + " has been born to " + progenitor?.name);
+      this.people.push(child);
+      this.livingPeople.push(child);
+      this.deadPlayers = this.deadPlayers.filter(name => name !== player);
+      this.livingPlayers.push(player);
+    }
+    else{
+      console.log("no more eligable progenitors this round");
+    }
   }
 
   killPlayer(player: string): void{
     this.livingPlayers = this.livingPlayers.filter(name => name !== player);
     this.deadPlayers.push(player);
+    this.livingPeople = this.livingPeople.filter(person => person.name !== player);
   }
 
-  updateFerility(people: Person[]){
-    people.forEach(person => {
-      
+  updateFerility(){
+    this.people.forEach(person => {
+      if (this.adultAge <= person.age && person.age < this.elderlyAge && 
+        person.alive && person.children.length <= this.maxNumChildren){
+        person.fertile = true;
+      } else {
+        person.fertile = false;
+      }
     });
   }
 
-  iterateTime(){
-    this.paused = !this.paused;
+  pauseButton(){
+    if(this.paused){
+      this.paused = !this.paused;
+    }
+    else{
+      this.people.forEach(person => {
+        person.age++;
+      });
+      this.updateFerility();
+      this.eligableProgenitors = [];
+      this.people.forEach(person => {
+        if(person.fertile){
+          this.eligableProgenitors.push(person);
+        }
+      });
+      this.eligableProgenitors.sort(() => Math.random() - 0.5);
+      this.paused = !this.paused;
+    }
+  }
 
+  getPersonByName(name: string): Person | null{
+    for(let i = 0; i < this.livingPeople.length; i++){
+      if(this.livingPeople[i].name === name){
+        return this.livingPeople[i]
+      }
+    }
+    return null;
   }
   
   ngOnInit(): void {
