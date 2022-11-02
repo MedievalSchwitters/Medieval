901fb424dd49567b293f3f5fce4a5dd1868eff55
Daniel Turnquist <turnq005@gmail.com>
2022-11-01 21:24:20 -0500

    added cards lookin good

--- medieval-angular/src/app/app.module.ts
+++ medieval-angular/src/app/app.module.ts
@@ -12,6 +12,7 @@ import {ButtonModule} from 'primeng/button';
 import {InputTextModule} from 'primeng/inputtext';
 import { ConfigComponent } from './config/config.component';
 import {InputNumberModule} from 'primeng/inputnumber';
+import {CardModule} from 'primeng/card';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import {InputNumberModule} from 'primeng/inputnumber';
     FormsModule,
     ButtonModule,
     InputTextModule,
-    InputNumberModule
+    InputNumberModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
